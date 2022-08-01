fbfa9d086f755da327f4f5026a22c25d06c7dc63
Dar-rius <mohamedtine17@gmail.com>
2022-08-01 14:55:34 +0000

    add new urls and views

--- geektech/urls.py
+++ geektech/urls.py
@@ -5,5 +5,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.index_view)
+    path("home/", views.index_view, name="home"),
+    path("contact/", views.contact_view, name="contact"),
+    path("propos/", views.propos_view, name="propos"),
+    path("shop/", views.shop_view, name="shop"),
+    path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
 ]
--- geektech/views.py
+++ geektech/views.py
@@ -3,4 +3,23 @@ from django.http import HttpResponse
 
 # Create your views hered
 def index_view(request):
-    return HttpResponse("Je teste")
+    return render(request, "page/home.html", {})
+
+def contact_view(request):
+    return render(request, "page/contact.html", {})
+
+
+def propos_view(request):
+    return render(request, "page/propos.html", {})
+
+
+def shop_view(request):
+    return render(request, "page/shop.html", {})
+
+
+def login_view(request):
+    return render(request, "page/login.html", {})
+    
+
+def signup_view(request):
+    return render(request, "page/signup.html", {})
