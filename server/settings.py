2e1402d1f047ecb4b3b68d8e4d0ef70b7cb7e163
Mohamed Tine <81871519+Dar-rius@users.noreply.github.com>
2022-08-19 13:50:24 +0000

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -47,9 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'geektech',
-    'tailwind',
-    'theme',
+    'geektech'
 ]
 
 MIDDLEWARE = [
@@ -140,12 +138,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# tailwindcss settings
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-
-TAILWIND_APP_NAME = 'theme'
-
-NPM_BIN_PATH = r"C:\Users\Lenovo\AppData\Roaming\npm";
